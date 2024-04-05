ea2464cef8617ff3c0ed22206f43557fbd9066d0
Giacomo Marciani <mgiacomo@amazon.com>
2024-04-05 13:30:34 +0200

    [Security] Use S3 backend in all examples to secure state files.
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

---
+++ examples/api_only/terraform.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    # The 'bucket' attribute has been intentionally omitted so that 'terraform init' will prompt the user to specify it.
+    # bucket = "INSERT_HERE_YOUR_BUCKET_NAME"
+    key    = "terraform-aws-parallelcluster/examples/api_only/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
